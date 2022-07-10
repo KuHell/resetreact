b55fbbffe83dcc3fc0bbb25a4eb7fc7e7ba778b6
Sang Hyun Park <itkdgus489@gmail.com>
2022-07-10 20:16:19 +0900

    Resetart React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [blogTit, setblogTit] = useState([
+    "react 수련",
+    "vue 수련",
+    "python 수련",
+  ]);
+  const [like, setlike] = useState(0);
+  const blogtitSet = () => {
+    let copy = [...blogTit];
+    copy[0] = "react 마스터";
+    setblogTit(copy);
+  };
+  const titSort = () => {
+    const sort = [...blogTit];
+    setblogTit(sort.sort());
+  };
+  const [showModal, setshowModal] = useState(false);
+  const modalShow = (value) => {
+    setshowModal(true);
+    settitle(value);
+  };
+  const [title, settitle] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navBar">
+        <p className="topTit">KuHell</p>
+      </div>
+      {blogTit.map(function (i, key) {
+        return (
+          <div
+            className="blogList"
+            key={key}
+            onClick={() => {
+              modalShow(key);
+            }}
+          >
+            <p className="blogTit">{i}</p>
+            <p className="blogDate">7월 10일</p>
+          </div>
+        );
+      })}
+      {showModal ? <Modal blogTit={blogTit} title={title} /> : null}
+      <input onClick={() => {}}></input>
+      <button>발행</button>
+    </div>
+  );
+}
+function Modal(props) {
+  return (
+    <div>
+      <p>{props.blogTit[props.title]}</p>
+      <p>날짜</p>
+      <p>상세 내용</p>
     </div>
   );
 }
