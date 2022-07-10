541fce3d35f0d891d56d15912214ec7d1245154e
Sang Hyun Park <itkdgus489@gmail.com>
2022-07-10 20:18:14 +0900

    nmk

---
+++ src/Component/Modal.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+function Modal() {
+  return (
+    <div>
+      <p>글 내용</p>
+      <p>날짜</p>
+      <p>상세 내용</p>
+    </div>
+  );
+}
+
+export default Modal;
